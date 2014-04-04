37f99fb08e1d480deb1b2c7e2dbf6ce888f76d12
Can ATAC <can.atac@gmail.com>
2014-04-04 04:10:29 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'cdc859cd256df04c2e4d8f55733dbd2d7f4f79979f18549d3ac0ceaa32c9f361921e6d7e451362c16a2baebd5daebef2d544c288dd06356d960cdb6571c67676'
